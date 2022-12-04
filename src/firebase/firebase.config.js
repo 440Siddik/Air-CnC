c1585a51a3d6f218e63a2db38bad2ece86981028
Afraaz <ahmedalif102@gmail.com>
2022-12-04 14:03:31 +0600

    first-commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQjczypL3jX2ReaSb4rAqL0l2OgySqjrg",
+  authDomain: "air-cnc-4612a.firebaseapp.com",
+  projectId: "air-cnc-4612a",
+  storageBucket: "air-cnc-4612a.appspot.com",
+  messagingSenderId: "238644522569",
+  appId: "1:238644522569:web:db3ac825e52bf8a5829998",
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
